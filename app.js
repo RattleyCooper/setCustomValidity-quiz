20db7d4d1e210324243fb0535964b87fdf4ee6e9
Wykleph <Wykleph@users.noreply.github.com>
2016-01-30 21:40:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var submit = document.querySelector('#submit');
 
 function validateInput(input){
     return [
-        [( firstPasswordInput.value != secondPasswordInput.value ), "The passwords must match."], // mismatch
-        [( input.value.length < 16 ), "The password should be at least 16 characters."], // under 16
-        [( input.value.length > 100 ), 'The password should not be over 100 characters'],  // over 100
-        [( !( /[\!\@\#\$\%\^\&\*]/g.test(input.value)) ), "The password must contain a following symbol :  !, @, #, $, %, ^, &, *"],  // no symbols
-        [( !( /[0-9]/g.test(input.value)) ), "The password must contain a number"], // no nums
-        [( !( /[a-z]/.test(input.value)) ), "The password must contain a lowercase letter."], // no lowers
-        [( !( /[A-Z]/.test(input.value)) ), "The password must contain an uppercase letter."], // no uppers
-        [( /[^A-z0-9\!\@\#\$\%\^\&\*]/g.test(input.value) ), "This password contains illegal characters."] // illegal chars
+        [( firstPasswordInput.value != secondPasswordInput.value ), "Passwords must match."], // mismatch
+        [( input.value.length < 16 ), "Password needs 16 characters."], // under 16
+        [( input.value.length > 100 ), 'Password should not exceed 100 characters'],  // over 100
+        [( !( /[\!\@\#\$\%\^\&\*]/g.test(input.value)) ), "Missing !, @, #, $, %, ^, &, or *"],  // no symbols
+        [( !( /[0-9]/g.test(input.value)) ), "Missing a number"], // no nums
+        [( !( /[a-z]/.test(input.value)) ), "Missing a lowercase letter."], // no lowers
+        [( !( /[A-Z]/.test(input.value)) ), "Missing an uppercase letter."], // no uppers
+        [( /[^A-z0-9\!\@\#\$\%\^\&\*]/g.test(input.value) ), "Password has illegal characters."] // illegal chars
     ];
 }
 
@@ -26,7 +26,6 @@ function assembleErrorList(validityArray){
         {
             errorMsgList.push(msg);
         }
-
     }
     return errorMsgList;
 }
